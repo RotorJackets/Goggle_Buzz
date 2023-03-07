1c950cfba87cbbbcc63dd5ede5d2957817ef55b3
Ian B <ianboraks@gmail.com>
2023-03-06 22:14:30 -0500

    Remvole of PyCharm trash

--- main.py
+++ main.py
@@ -10,26 +10,28 @@ from api_key import bot_token
 # Testing Server id = 473695678690885632
 # RotorJackets id = 723199784697200810
 
-guilds = [discord.Object(id = 473695678690885632), discord.Object(id = 723199784697200810)]
+guilds = [discord.Object(id = 473695678690885632), discord.Object(id =723199784697200810)]
 
 class client(discord.Client):
     def __init__(self):
         super().__init__(intents=discord.Intents.all())
         self.synced = False
-
+    
     async def on_ready(self):
         await self.wait_until_ready()
         if not self.synced:
             #await tree.sync(guilds)
             self.synced = True
         print(f"We have logged in as: {self.user}.")
-        await self.change_presence(
-
-        )
+        
 bot = client()
 tree = app_commands.CommandTree(bot)
 
 #Commands
+@tree.command(name = "reward", description = "Give me a reward", guilds = guilds)
+async def self(interaction: discord.Interaction):
+    await interaction.response.send_message("Salt reloaded. It's time to be a fucking menace.")
+    print("Reward command used.")
 
 
 bot.run(bot_token)

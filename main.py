411a5ce677ac5873aafa16fc95b54b1bdf260631
Ian B <ianboraks@gmail.com>
2023-03-09 10:52:21 -0500

    Updated Master with correct sync commands

--- main.py
+++ main.py
@@ -15,21 +15,25 @@ guilds = [discord.Object(id = 473695678690885632), discord.Object(id =7231997846
 class client(discord.Client):
     def __init__(self):
         super().__init__(intents=discord.Intents.all())
-        self.synced = False
-    
+
     async def on_ready(self):
         await self.wait_until_ready()
-        if not self.synced:
-            await tree.sync( guild= discord.Object(id = 473695678690885632))
-            self.synced = True
         print(f"We have logged in as: {self.user}.")
-        
+
+
 bot = client()
 tree = app_commands.CommandTree(bot)
 
-#Commands
-@tree.command(name = "flyday", description = "Find the next fly day", guilds= guilds)
-async def self(interaction: discord.Interaction):
-    await interaction.response.send_message("Salt reloaded. It's time to be a fucking menace.")
+
+# Commands
+@bot.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if "!sync" in message.content.lower():
+        await tree.sync()
+        await message.channel.send("Synced")
+        print("Command tree synced.")
 
 bot.run(bot_token)

0c123f4561072afe28778bb95c37959998bb2a9f
SolarBlackhole <dwyckoff3@gatech.edu>
2023-03-24 23:39:19 -0400

    Updated Role Selection & Shipping
    Role Selection embed had all roles and updated role removed messages.
    
    Shipping Threads now have embeds to close orders.

--- cogs/util/role_selection.py
+++ cogs/util/role_selection.py
@@ -16,6 +16,7 @@ class RoleOptions(discord.ui.View):
     @discord.ui.button(
         label="@ GT",
         style=discord.ButtonStyle.grey,
+        emoji="<:gogglesbuzz:753465023560679484>",
     )
     async def option_one(
         self, interaction: discord.Interaction, button: discord.ui.button
@@ -23,7 +24,7 @@ class RoleOptions(discord.ui.View):
         rotor = get(interaction.guild.roles, name="RotorJacket")
         if rotor in interaction.user.roles:
             await interaction.user.remove_roles(rotor)
-            await interaction.response.send_message("Sadge", ephemeral=True)
+            await interaction.response.send_message("RotorJacket role removed", ephemeral=True)
         else:
             await interaction.user.add_roles(rotor)
             await interaction.response.send_message(
@@ -34,6 +35,7 @@ class RoleOptions(discord.ui.View):
     @discord.ui.button(
         label="Not @ GT",
         style=discord.ButtonStyle.grey,
+        emoji="<a:bigthink:753469546526146632>",
     )
     async def option_2(
         self, interaction: discord.Interaction, button: discord.ui.button
@@ -41,13 +43,45 @@ class RoleOptions(discord.ui.View):
         friend = get(interaction.guild.roles, name="Friend of Rotorjackets")
         if friend in interaction.user.roles:
             await interaction.user.remove_roles(friend)
-            await interaction.response.send_message("Goodbye Friend", ephemeral=True)
+            await interaction.response.send_message("Friend of RotorJackets role removed", ephemeral=True)
         else:
             await interaction.user.add_roles(friend)
             await interaction.response.send_message(
                 "https://cdn.discordapp.com/attachments/534939044078157834/1086736052670173295/IMG_3452.jpg",
                 ephemeral=True,
             )
+
+    @discord.ui.button(
+        label="Racer",
+        style=discord.ButtonStyle.grey,
+        emoji = "🏁",
+    )
+    async def option_3(
+        self, interaction: discord.Interaction, button: discord.ui.button
+    ):
+        racer = get(interaction.guild.roles, name="racer")
+        if racer in interaction.user.roles:
+            await interaction.user.remove_roles(racer)
+            await interaction.response.send_message("Racer Role removed", ephemeral=True)
+        else:
+            await interaction.user.add_roles(racer)
+            await interaction.response.send_message("Pilots arm your quads", ephemeral=True)
+
+    @discord.ui.button(
+        label="Sim Pings",
+        style=discord.ButtonStyle.grey,
+        emoji="📺",
+    )
+    async def option_4(
+            self, interaction: discord.Interaction, button: discord.ui.button
+    ):
+        sim = get(interaction.guild.roles, name="sim")
+        if sim in interaction.user.roles:
+            await interaction.user.remove_roles(sim)
+            await interaction.response.send_message("Sim Role removed", ephemeral=True)
+        else:
+            await interaction.user.add_roles(sim)
+            await interaction.response.send_message("Update your Velocidrone", ephemeral=True)
 
 
 @app_commands.guild_only()
@@ -58,18 +92,20 @@ class RoleSelection(commands.Cog):
     @app_commands.command(
         name="intro",
         description="Generate new intro message",
+
     )
     async def intro(self, interaction: discord.Interaction):
-        welcome_channel = get(interaction.guild.text_channels, name="welcome")
-        embed = discord.Embed(color=discord.Color.random())
+        welcome_channel = self.bot.get_channel(config["welcome_channel"])
+        embed = discord.Embed(color=discord.Color.gold())
         embed.set_author(name=f"Welcome to RotorJackets")
+        embed.set_thumbnail(url=(welcome_channel.guild.icon))
         embed.add_field(
             name="This server is the primary point of communication for RotorJackets.",
             value="Open to all who love drones\n\n"
             "If you want to race please join our MultiGP chapter here: https://www.multigp.com/chapters/view/?chapter=RotorJackets\n\n"
-            "1. Post an introduction in introductions\n"
-            "2. Checkout new-member-info\n"
-            "3. Any FPV video you want to plug, you can do so in shameless-plug\n\n"
+            "1. Post an introduction in #introductions\n"
+            "2. Checkout #new-member-info\n"
+            "3. Any FPV video you want to plug, you can do so in #shameless-plug\n\n"
             "Select your roles with the buttons below",
         )
 
--- cogs/util/shipping.py
+++ cogs/util/shipping.py
@@ -7,6 +7,37 @@ from config import config as config_main
 
 config = config_main["util"]
 
+class OrderOptions(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
+
+    @discord.ui.button(
+        label="Goods Ordered",
+        style=discord.ButtonStyle.green,
+        emoji="<a:Verify:940328302420295730>",
+    )
+    async def option_one(
+        self, interaction: discord.Interaction, button: discord.ui.button
+    ):
+        shipping_channel = get(interaction.guild.text_channels, name="shipping-sharing")
+        await interaction.channel.edit(locked=True)
+        await shipping_channel.send(f"Goods for {interaction.user}'s group order has been ordered!")
+        pass
+
+    @discord.ui.button(
+        label="Cancel Order",
+        style=discord.ButtonStyle.danger,
+        emoji="⛔",
+    )
+    async def option_two(
+            self, interaction: discord.Interaction, button: discord.ui.button
+    ):
+        shipping_channel = get(interaction.guild.text_channels, name="shipping-sharing")
+        print(interaction.channel)
+        await interaction.channel.edit(locked=True)
+        await shipping_channel.send(f"{interaction.user}'s group order has been canceled!")
+
 @app_commands.guild_only()
 class Shipping(commands.Cog):
     def __init__(self, bot) -> None:
@@ -19,10 +50,16 @@ class Shipping(commands.Cog):
 
         shipping_channel = get(message.guild.text_channels, name="shipping-sharing")
         if message.channel.id == shipping_channel.id:
-            await message.channel.create_thread(
+            thread = await message.channel.create_thread(
                 name=f"{message.author} is ordering goods",
                 type=ChannelType.public_thread,
             )
+            embed = discord.Embed(color=discord.Color.gold(),
+                                  title="Group Order",
+                                  description="Complete your group order with the buttons below")
+            view = OrderOptions()
+            msg = await thread.send(embed=embed, view=view)
+            await msg.pin()
 
     @app_commands.command(
         name="new_order",
@@ -30,6 +67,7 @@ class Shipping(commands.Cog):
     )
     async def new_order(self, interaction: discord.Interaction, website: str = None):
         role = get(interaction.guild.roles, name=config["order_role"])
+        thread = 0
         if role not in interaction.user.roles:
             await interaction.response.send_message(
                 "You must be a RotorJacket to create a group order"
@@ -40,16 +78,26 @@ class Shipping(commands.Cog):
                 interaction.guild.text_channels, name=config["shipping_channel"]
             )
             if website == None:
-                await channel.create_thread(
+                thread = await channel.create_thread(
                     name=f"{interaction.user} is ordering goods",
                     type=ChannelType.public_thread,
                 )
             else:
-                await channel.create_thread(
+                thread = await channel.create_thread(
                     name=f"{interaction.user} is ordering goods from {website}",
                     type=ChannelType.public_thread,
                 )
             await interaction.response.send_message(f"Group order created in {channel}")
+            embed = discord.Embed(color=discord.Color.gold(),
+                                  title="Group Order",
+                                  description="Complete your group order with the buttons below")
+            view = OrderOptions()
+            msg = await thread.send(embed=embed, view=view)
+            await msg.pin()
+
+
+
+
 
 
 async def setup(bot: commands.Bot) -> None:
